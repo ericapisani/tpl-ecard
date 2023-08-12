85c3bb5ca3a730a3c68978c9abee04b94f44aa08
Erica Pisani <pisani.erica@gmail.com>
2023-08-11 21:16:47 -0400

    Add some dependencies for interacting with local storage.
    Start adding some UI elements

--- App.js
+++ App.js
@@ -1,10 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, View, TextInput, StyleSheet } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React from 'react'
 
 export default function App() {
+  const [number, onChangeNumber] = React.useState(null);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('library-card-number');
+      if (value !== null) {
+        // value previously stored
+        console.log('Here is the value: ', value)
+      }
+    } catch (e) {
+      // error reading value
+      console.log('There was an issue reading the value:', e)
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput 
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="Enter your library card number here!"
+        keyboardType="numeric"
+        style={styles.libraryCardNumberInput}
+      />
+      <Button
+        onPress={() => {}}
+        title="Save library card number"
+        accessibilityLabel="Save library card number"
+        style={styles.saveButton}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +48,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
   },
+  libraryCardNumberInput: {
+    height: 80,
+    width: "75%",
+    borderWidth: 1,
+    textAlign: "center"
+  }
 });
