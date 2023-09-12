cd3c98d219dc6cd3afbdf09680f1360d1b9d61ac
Erica Pisani <pisani.erica@gmail.com>
2023-09-11 20:53:15 -0400

    Remove old code

--- App.js
+++ App.js
@@ -45,53 +45,6 @@ export default function App() {
     }
   }
 
-  function BarcodeChunk(props) {
-    const { binary, padding, options } = props;
-
-    // Creates the barcode out of the encoded binary
-    let yFrom;
-    if (options.textPosition == 'top') {
-      yFrom = options.fontSize + options.textMargin;
-    } else {
-      yFrom = 0;
-    }
-
-    let barWidth = 0;
-    let x = 0;
-    let bars = [];
-    for (var b = 0; b < binary.length; b++) {
-      x = b * options.width + padding;
-
-      if (binary[b] === '1') {
-        barWidth++;
-      } else if (barWidth > 0) {
-        bars.push({
-          x: x - options.width * barWidth,
-          y: yFrom,
-          width: options.width * barWidth,
-          height: options.height,
-        });
-        barWidth = 0;
-      }
-    }
-
-    // Last draw is needed since the barcode ends with 1
-    if (barWidth > 0) {
-      bars.push({
-        x: x - options.width * (barWidth - 1),
-        y: yFrom,
-        width: options.width * barWidth,
-        height: options.height,
-      });
-    }
-
-    return bars.map((bar, i) => {
-      return (
-        <Rect key={i} x={bar.x} y={bar.y} width={bar.width} height={bar.height} />
-      );
-    });
-  }
-
   return (
     <View style={styles.container}>
       {libraryCardNumber && (
